b20e6e594e85dd8e8023cf2b9d7b2ba98e3f62cb
luisballagas <luiso96ballagas@gmail.com>
2024-01-23 12:15:01 -0500

    feat: add empty component to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client"
-import Image from "next/image";
+import { Empty } from 'antd';
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
     </main>
   );
 }
